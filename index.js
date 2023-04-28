ddd0dd9cc22c8b04b33b775ad8e3f4baeed1ae28
slayerpapillon <slayerpapillon@gmail.com>
2023-04-28 19:19:43 +0200

    New functions issuesEvent & sendSMS into index.js

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const core = require('@actions/core');
 
+let freemobile_user = "";
+let freemobile_password = "";
+
 const main = async () => {
 	try {
 		const event_name = core.getInput('event_name', { required: true });
 		const event = core.getInput('event', { required: true });
-		const freemobile_user = core.getInput('freemobile_user', { required: true });
-		const freemobile_password = core.getInput('freemobile_password', { required: true });
+		freemobile_user = core.getInput('freemobile_user', { required: true });
+		freemobile_password = core.getInput('freemobile_password', { required: true });
 
         switch (event_name) {
             case 'discussion':
@@ -15,7 +18,9 @@ const main = async () => {
             case 'issue_comment':
             break;
             case 'issues':
-            break;
+                const status = await issuesEvent(event);
+                status == 200 ? core.notice('Send OK') : core.setFailed('Send error!');
+                return;
             case 'pull_request':
             break;
             case 'pull_request_review':
@@ -31,6 +36,35 @@ const main = async () => {
 	catch (error) {
 		core.setFailed(error.message);
   }
+}
+
+async function issuesEvent(event) {
+    const action = event.action; // Ex: opened
+    const repo = event.repository.full_name; // Ex: octocat/Hello-World
+    const sender = event.sender.login; // Ex: octocat
+    const number = event.issue.number; // Ex: 1
+    const title = event.issue.title; // Ex: My pull title
+
+    const message = 'GitHub Issues event ' + action + '\nOn repo ' + repo + " by " + sender + "\nIssue #" + number + " " + title;
+    return await sendSMS(message);
+}
+
+
+async function sendSMS(message) {
+    const response = await fetch("https://smsapi.free-mobile.fr/sendmsg", {
+        method: "POST",
+        body: JSON.stringify({
+            user: freemobile_user,
+            pass: freemobile_password,
+            msg: message
+        }),
+        headers: {
+        "Content-type": "application/json; charset=UTF-8"
+        }
+    });
+
+    const status = response.status;
+    return status;
 }
 
 // Call the main function to run the action
